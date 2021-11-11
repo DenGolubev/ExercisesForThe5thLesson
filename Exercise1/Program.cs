c3f52af5b5ab7a952a85955f832aecaed71f0765
Денис Голубев <amigo36@yandex.ru>
2021-11-11 18:02:34 +0300

    Выполнено

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,7 +6,45 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RationalNumber rationalNumber;
+
+            var numbers = EnterNumber();
+
+            rationalNumber = new RationalNumber(numbers.Item1, numbers.Item2);
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Дробь - {rationalNumber}");
+            Console.WriteLine($"Сложение - {rationalNumber.Numerator + rationalNumber.Denominator}");
+            Console.WriteLine($"Вычитание - {rationalNumber.Numerator - rationalNumber.Denominator}");
+            Console.WriteLine($"Умножение - {rationalNumber.Numerator * rationalNumber.Denominator}");
+            Console.WriteLine($"Деление - {rationalNumber.Numerator / rationalNumber.Denominator}");
+            Console.WriteLine($"Остаток от Деления - {rationalNumber.Numerator % rationalNumber.Denominator}");
+            Console.WriteLine($"Постфиксный Инкримент - {rationalNumber++}");
+            Console.WriteLine($"Постфиксный Декримент - {rationalNumber--}");
+            Console.WriteLine($"Префиксный Инкримент - {++rationalNumber}");
+            Console.WriteLine($"Префиксный Декримент - {--rationalNumber}");
+            Console.WriteLine($"Равенство - {rationalNumber.Numerator == rationalNumber.Denominator}");
+            Console.WriteLine($"Не равны - {rationalNumber.Numerator != rationalNumber.Denominator}");
+            Console.WriteLine($"Числитель Больше Знаменателя - {rationalNumber.Numerator > rationalNumber.Denominator}");
+            Console.WriteLine($"Числитель Меньше Знаменателя - {rationalNumber.Numerator < rationalNumber.Denominator}");
+            Console.WriteLine($"Числитель Больше или равен Знаменателю - {rationalNumber.Numerator >= rationalNumber.Denominator}");
+            Console.WriteLine($"Числитель Меньше или равен Знаменателю - {rationalNumber.Numerator <= rationalNumber.Denominator}");
+            Console.WriteLine($"Преобразование float - {(float)rationalNumber}");
+
+            Console.ReadKey();
+        }
+
+        static (int, int) EnterNumber()
+        {
+            Console.WriteLine("Введите числитель");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знаменатель");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            var numbers = (n1, n2);
+
+            return numbers;
         }
     }
 }
--- Exercise1/RationalNumber.cs
+++ Exercise1/RationalNumber.cs
@@ -6,7 +6,104 @@ using System.Threading.Tasks;
 
 namespace Exercise1
 {
-    class RationalNumber
+    public class RationalNumber
     {
+        double numerator => Numerator;
+        double denominator => Denominator;
+
+        public double Numerator { get; }
+        public double Denominator { get; }
+        public RationalNumber(double numerator, double denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static bool operator !=(RationalNumber n1, RationalNumber n2)
+        {
+            return !n1.Equals(n2);
+        }
+
+        public static bool operator ==(RationalNumber n1, RationalNumber n2)
+        {
+            return n1.Equals(n2);
+        }
+
+        public static int operator >(RationalNumber n1, RationalNumber n2)
+        {
+            return n1.Equals(n2).CompareTo(n2);
+        }
+
+        public static int operator <(RationalNumber n1, RationalNumber n2)
+        {
+            return n1.Equals(n2).CompareTo(n2);
+        }
+
+        public static int operator >=(RationalNumber n1, RationalNumber n2)
+        {
+            return n1.Equals(n2).CompareTo(n2);
+        }
+
+        public static int operator <=(RationalNumber n1, RationalNumber n2)
+        {
+            return n1.Equals(n2).CompareTo(n2);
+        }
+
+        public static RationalNumber operator +(RationalNumber n1, RationalNumber n2)
+        {
+            return new RationalNumber(n1.numerator + n2.numerator, n1.denominator + n2.denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber n1, RationalNumber n2)
+        {
+            return new RationalNumber(n1.numerator - n2.numerator, n1.denominator - n2.denominator);
+        }
+
+        public static RationalNumber operator *(RationalNumber n1, RationalNumber n2)
+        {
+            return new RationalNumber(n1.numerator * n2.numerator, n1.denominator * n2.denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber n1, RationalNumber n2)
+        {
+            return new RationalNumber(n1.numerator / n2.numerator, n1.denominator / n2.denominator);
+        }
+
+        public static RationalNumber operator ++(RationalNumber n1)
+        {
+            return new RationalNumber(n1.numerator + 1, n1.denominator + 1);
+        }
+
+        public static RationalNumber operator --(RationalNumber n1)
+        {
+            return new RationalNumber(n1.numerator - 1, n1.denominator - 1);
+        }
+
+        public static RationalNumber operator %(RationalNumber n1, RationalNumber n2)
+        {
+            return new RationalNumber(n1.numerator - (n1.numerator / n2.numerator) * n2.denominator, n1.denominator - (n1.denominator / n2.denominator) * n2.denominator);
+        }
+
+        public static implicit operator float(RationalNumber n1)
+        {
+            var x = new RationalNumber(n1.numerator, n1.denominator);
+            return (float)x.numerator;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.numerator} / {this.denominator}";
+        }
     }
 }
